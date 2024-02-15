a3b16719f2ef4c7f37396d7907089b389376621f
RayanneO <rohara@csus.edu>
2024-02-15 02:31:22 -0500

    Add files via upload

--- wzip.c
+++ wzip.c
@@ -18,40 +18,26 @@ int main(int argc, char *argv[]) {
   //looks through files
   for (int i = 1; i < argc; i++) {
     FILE *fp = fopen(argv[i], "r"); // open file
-    if (fp == NULL) {
-      printf("wzip: cannot open file\n");
-      exit(1);
+    int first_c = fgetc(fp);
+    if (first_c == EOF) {
+      fclose(fp);
     }
-    //defining variables
-    int count = 0;
-    char buffer[BUFFER_SIZE];
-    int first_c;
-    size_t bytes_read;
-    //continues until fread() returns 0
-    while((bytes_read = fread(buffer, sizeof(char), BUFFER_SIZE, fp)) > 0) {
-      for (size_t j = 0; j < bytes_read; j++) {
-        //if count is zero, initialize first_c and increment count
-        if (count == 0) {
-          first_c = buffer[j];
+    else {
+      int count = 1;
+      int current_c;
+      while ((current_c = fgetc(fp)) != EOF) {
+        if (current_c == first_c) {
           count++;
         }
-        //if current character is same as first_c, increment count
-        else if  (first_c == buffer[j]) {
-          count++;
-        }
-        // if current character is not same as first_c, compress and reset count and first_c
         else {
-          compression(count, first_c);
+          fwrite(&count, sizeof(int), 1, stdout);
+          putchar(first_c);
           count = 1;
-          first_c = buffer[j];
+          first_c = current_c;
         }
       }
+      fclose(fp);
     }
-    //write any remaining count and character
-    if (count > 0) {
-      compression(count, first_c);
-    }
-  fclose(fp);
   }
-  exit(0);
+  exit(0
 }
